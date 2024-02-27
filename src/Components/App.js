0dd19ec1463754e330ead85119702df5ea698f04
Monet <monetpaul23@gmail.com>
2024-02-26 19:06:33 -0500

    restarting the project  over

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,22 +1,9 @@
-import Editor from './Editor';
 
 function App() {
-  return ( <>
+  return ( 
    
-   <div className="pane top-pane">
-    < Editor  displayName={'HTML'}/>
-    < Editor  displayName={'CSS'}/>
-    < Editor   displayName={'JS'}/>
-   </div>
-   <div className="pane"> 
-   <iframe title="output" 
-           sandbox="allow scripts"
-           frameBorder="0"
-           width="100%"
-           height="100%" />
-   </div>
+  <div className="w-screen h-screen flex items-center justify-center ">test </div>
   
-  </>
     
   );
 }
